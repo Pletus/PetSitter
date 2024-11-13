64a2972308763e3974c808364c33e1c92153e1b1
YagoPazos <yagopb@gmail.com>
2024-11-13 16:08:58 +0100

    working on backend availability controllers

--- backend/src/routes/availabilityRoutes.ts
+++ backend/src/routes/availabilityRoutes.ts
@@ -1,18 +1,17 @@
-import express from 'express';
-import { getAvailability, createAvailability, updateAvailability, deleteAvailability } from '../controllers/availabilityController';
+import express from "express";
+import {
+  getAvailability,
+  createAvailability,
+  updateAvailability,
+  deleteAvailability,
+} from "../controllers/availabilityController";
 
 const router = express.Router();
 
-// Ruta para obtener todos los horarios disponibles
-router.get('/availability', getAvailability);
-
-// Ruta para crear un nuevo horario de disponibilidad
-router.post('/availability', createAvailability);
-
-// Ruta para actualizar la disponibilidad (marcar como disponible/no disponible)
-router.put('/availability/:id', updateAvailability);
-
-// Ruta para eliminar un horario de disponibilidad
-router.delete('/availability/:id', deleteAvailability);
+router
+  .get("/availability", getAvailability)
+  .post("/availability", createAvailability)
+  .put("/availability/:id", updateAvailability)
+  .delete("/availability/:id", deleteAvailability);
 
 export default router;
